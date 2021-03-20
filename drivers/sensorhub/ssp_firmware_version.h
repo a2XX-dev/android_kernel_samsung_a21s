3506f1fb5af4fa9c1cd95d7487371e1a6c1c869c
durasame <daffetyxd@gmail.com>
2021-03-20 13:33:29 -0300

    Initial commit (Import from: A217FXXU5BUB4)
    Signed-off-by: durasame <daffetyxd@gmail.com>
    Change-Id: I1ba1a2147dc2ef64d77f885ae092a5f5c4169091

---
+++ drivers/sensorhub/ssp_firmware_version.h
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (C) 2019, Samsung Electronics Co. Ltd. All Rights Reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ */
+
+#ifdef CONFIG_SENSORS_SSP_A21S
+#define SSP_FIRMWARE_REVISION       20092900
+#else
+#define SSP_FIRMWARE_REVISION       20040300
+#endif
